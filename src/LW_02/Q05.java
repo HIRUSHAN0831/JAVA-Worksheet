96cb489d35bdd451835672fac8112bf2bd51a0be
HIRUSHAN0831 <hirushanpramuditha36@gmail.com>
2025-09-22 21:04:11 +0530

    answer for Q5

---
+++ src/LW_02/Q05.java
@@ -0,0 +1,29 @@
+package LW_02;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Q05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a sentence:");
+        String input = sc.nextLine();
+
+        StringTokenizer st = new StringTokenizer(input, " ,.!?;:-'\"");
+        String cleaned = "";
+
+        while (st.hasMoreTokens()) {
+            cleaned += st.nextToken().toLowerCase();
+        }
+
+        String reversed = new StringBuilder(cleaned).reverse().toString();
+
+        if (cleaned.equals(reversed)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a palindrome");
+        }
+
+        sc.close();
+    }
+}
